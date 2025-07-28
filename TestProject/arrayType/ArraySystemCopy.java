f10729e8b3791506adc8455b249c0da3b064a5b7
Lim Eung Gyu <grtleg002@gmail.com>
2025-07-28 22:26:51 +0900

    SystemArrayCopy
    원본배열의, 어디서부터, 어디 배열의, 어디서부터, 몇개나  복살할건지 쓰는 배열 복사

---
+++ TestProject/arrayType/ArraySystemCopy.java
@@ -0,0 +1,25 @@
+package arrayType;
+
+public class ArraySystemCopy {
+
+	public static void main(String[] args) {
+		// System.arraycopy(원본, 시작인덱스, 대상, 시작인덱스, 복사할 개수)
+		int[]a= {1,2,3,4};
+		int[]b= new int[a.length];
+		System.arraycopy(a, 0, b, 1, 3);
+		//목적지 배열의 인덱스값 확인해야함 (out of bounds)
+		//System.arraycopy(a, 0, b, 1, a.length); //b배열의 크기가 4인데 1번인덱스부터 4개를 붙여넣으면 오류 발생
+		//arraycopy: last destination index 5 out of bounds for int[4]
+		
+		for(int i = 0; i<a.length; i++) {
+			System.out.print(a[i]+ " ");
+			
+		}
+		System.out.println();
+		for(int i = 0; i<a.length; i++) {
+			System.out.print(b[i]+ " ");
+			
+		}
+	}
+
+}
